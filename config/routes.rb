f65d286d7ea420850c4bb3d9e9fd16a09568da8c
Ahmed Nadir <28578413+TechnoVen@users.noreply.github.com>
2025-12-02 15:47:18 +0100

    Small fixes for Scope1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :workout_plans do
     resources :ai_messages, only: [:index, :create]
+    resources :workout_exercises, except: [:index, :show]
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
