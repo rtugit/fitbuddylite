3d19b47011b54a9e02c1321efffbaadab79d7059
Leonardo Van den Berghe <leonardovdb.lvdb@gmail.com>
2025-12-03 15:38:05 +0100

    linking and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  # Allow GET requests for sign out (in addition to DELEE which is already built in in devise_for:users) as we want to get back to the home page after signing out
+  devise_scope :user do
+   get "/users/sign_out", to: "devise/sessions#destroy"
+  end
+
+
   resources :workout_plans do
     resources :ai_messages, only: [:index, :create]
     resources :workout_exercises, except: [:index, :show]
